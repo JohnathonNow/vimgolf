35ed901d44ee14cc35a5dee64d1fb397daa1b32a
Breanna <bdevorem@nd.edu>
2017-03-20 18:27:38 -0400

    backend serves the holes

--- backend/project.clj
+++ backend/project.clj
@@ -1,9 +1,11 @@
 (defproject backend "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Backend for Vim Golf"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+:dependencies [[org.clojure/clojure "1.8.0"]
+                 [aleph "0.4.2-alpha6"]
+                 ]
   :main ^:skip-aot backend.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- backend/src/backend/core.clj
+++ backend/src/backend/core.clj
@@ -1,5 +1,6 @@
 (ns backend.core
   (:gen-class))
+(require '[aleph.http :as http])
 (require '[clojure.string :as str])
 
 ;; replaces the special keystrokes, like \\<esc>, with the letter E
@@ -11,6 +12,32 @@
       (str/replace 
         (str/replace string #"\\<\S{0,4}>" "E") #"\\\\" "S"))))
 
+
+(defn handler 
+  [req]
+  (println req)
+  (let [reply {:status 200
+               :headers {"content-type" "text/html"}
+               :body "hello!"}]
+    (case (:uri req) 
+      "/"  (assoc reply :body (slurp "../frontend/index.html"))
+      "/jquery.js" (assoc reply :body (slurp "../frontend/jquery.js"))
+      "/k"     (do (println (slurp(:body req)))
+               (assoc reply :body "{\"status\": \"success\"}"))
+      ;default
+            (do  
+              (if (or 
+                    (re-matches #"/start/h\d+" (:uri req))
+                    (re-matches #"/end/h\d+" (:uri req)))
+                  (assoc 
+                    (assoc reply :body (slurp (str "../holes" (:uri req) ".txt")))
+                     :headers {"content-type" "text/plain"})))
+    )
+  )
+)
+
 (defn -main
   [& args]
-  (println (count-keystrokes (first args))))
+  (.wait-for-close (http/start-server handler {:port 8080}))
+  ;(println (count-keystrokes (first args))))
+)
