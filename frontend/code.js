bd23095ee11df94666fe52667102e1e02fc032c2
Breanna <bdevorem@nd.edu>
2017-03-22 12:45:58 -0400

    end/start code displayed on dropdown click. formatting TBD

--- frontend/code.js
+++ frontend/code.js
@@ -12,29 +12,36 @@ var CodePanel = {
     }
 }
 CodePanel.__proto__ = Item;
-CodePanel.createCode("this will be a code panel", "holeCode");
+CodePanel.createCode("this will be a well with start code", "holeStart");
+CodePanel.addToDocument();
+
+CodePanel.createCode("this will be a well with end code", "holeEnd");
 CodePanel.addToDocument();
 
 window.changeCode = function(e){
     if(e.value=="hole1"){
         //code1.textContent = "Hole 1 eventually..."
-		populatePre('/start/h1');
+		populatePre('holeStart', '/start/h1');
+		populatePre('holeEnd', '/end/h1');
     }
     else if(e.value=="hole2"){
-		populatePre('/start/h2');
+		populatePre('holeStart', '/start/h2');
+		populatePre('holeEnd', '/end/h2');
     }
     else if(e.value == "hole3"){
-		populatePre('/start/h3');
+		populatePre('holeStart', '/start/h3');
+		populatePre('holeEnd', '/end/h3');
     }
     else if(e.value == "hole4"){
-		populatePre('/start/h4');
+		populatePre('holeStart', '/start/h4');
+		populatePre('holeEnd', '/end/h4');
     }
 }
 
-function populatePre(url) {
+function populatePre(id, url) {
     var xhr = new XMLHttpRequest();
     xhr.onload = function () {
-        document.getElementById('holeCode').textContent = this.responseText;
+        document.getElementById(id).textContent = this.responseText;
     };
     xhr.open('GET', url);
     xhr.send();
